f9d7fea264eda8e65d64c74793b8a0bebcfa2c67
nnnnQT <cortana@s007xcpu2552.lan>
2023-04-23 21:00:33 -0500

    added headers open

--- netlify/functions/open-ai-messenger/open-ai-messenger.js
+++ netlify/functions/open-ai-messenger/open-ai-messenger.js
@@ -1,9 +1,17 @@
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require('openai');
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+};
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
+    console.log(event);
     const body = JSON.parse(event.body);
+    console.log(body);
     const text = JSON.stringify(body.text);
 
     const configuration = new Configuration({
@@ -11,22 +19,32 @@ const handler = async (event) => {
     });
     const openai = new OpenAIApi(configuration);
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: 'text-davinci-003',
       prompt: text,
       temperature: 0,
       max_tokens: 42,
     });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ response: response.data.choices[0].text }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    console.log(response.data.choices[0].text);
+    // if (event.httpMethod === "OPTIONS") {
+    //   return {
+    //     statusCode: 200,
+    //     headers,
+    //     body: JSON.stringify({ message: "Successful preflight call." }),
+    //   };
+    // } else if (event.httpMethod === "POST") {
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({ response: response.data.choices[0].text }),
+        // // more keys you can return:
+        // headers: { "headerName": "headerValue", ... },
+        // isBase64Encoded: true,
+      };
+    // }
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
