7e926ef76060fabde307a6896ae3a3db8bdf9dc9
nnnnQT <cortana@s007xcpu2552.lan>
2023-04-23 20:12:15 -0500

    added openai

--- netlify/functions/open-ai-messenger/open-ai-messenger.js
+++ netlify/functions/open-ai-messenger/open-ai-messenger.js
@@ -1,22 +1,25 @@
-// const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    // const configuration = new Configuration({
-    //   apiKey: process.env.OPEN_AI_KEY,
-    // });
-    // const response = await openai.createCompletion({
-    //   model: "text-davinci-003",
-    //   prompt: event.body.text,
-    //   temperature: 0,
-    //   max_tokens: 42,
-    // });
+    const body = JSON.parse(event.body);
+    const text = JSON.stringify(body.text);
 
-    const subject = event.queryStringParameters.name || 'World'
+    const configuration = new Configuration({
+      apiKey: process.env.OPEN_AI_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0,
+      max_tokens: 42,
+    });
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ response: response.data.choices[0].text }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
